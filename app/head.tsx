d634af414e7420170d9332190cc060852879cbb1
hasan199191 <hasanacikgoz91@gmail.com>
2025-04-19 01:22:31 +0300

    fix: favicon dosya yolları düzeltildi

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,12 @@ export default function Head() {
       <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
       <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
       <link rel="manifest" href="/site.webmanifest" />
+      <link 
+        rel="icon" 
+        type="image/png"
+        sizes="192x192" 
+        href="/favicon/android-icon-192x192.png" 
+      />
     </>
   )
 }
